3a2b43de6fd7a2963fcebd3a69d27f909cd194c7
Jor Sargsyan <jor@gmail.com>
2022-08-27 22:53:20 +0400

    first cargo proj initialization

---
+++ my_proj/target/debug/deps/my_proj-87b4a226f62ae5db.d
@@ -0,0 +1,5 @@
+c:\Users\Jor\Desktop\rust_tutor\my_proj\target\debug\deps\my_proj-87b4a226f62ae5db.rmeta: src\main.rs
+
+c:\Users\Jor\Desktop\rust_tutor\my_proj\target\debug\deps\my_proj-87b4a226f62ae5db.d: src\main.rs
+
+src\main.rs:
---
+++ my_proj/target/debug/deps/my_proj-9f47a83ecfc6ac22.d
@@ -0,0 +1,5 @@
+c:\Users\Jor\Desktop\rust_tutor\my_proj\target\debug\deps\my_proj-9f47a83ecfc6ac22.rmeta: src\main.rs
+
+c:\Users\Jor\Desktop\rust_tutor\my_proj\target\debug\deps\my_proj-9f47a83ecfc6ac22.d: src\main.rs
+
+src\main.rs:
